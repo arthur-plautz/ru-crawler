113e157e623b83885c43b784d59141a550942d6c
Ventura <arthur.plautz@indicium.tech>
2022-05-31 10:32:53 -0300

    Changing Schedule Hour

--- airflow/dags/ru_crawler.py
+++ airflow/dags/ru_crawler.py
@@ -40,7 +40,7 @@ def build_reservation_task(target_meal, email, name, password):
 with DAG(
     'ru_meal_reservation',
     default_args=default_args,
-    schedule_interval="0 6 * * *",
+    schedule_interval="0 16 * * *",
     catchup=True
 ) as dag:
 
